e34163a9e9bdeb06f7b8df06ab75a6bc80b4d045
Vikash Shakya <vikashshakya367@gmail.com>
2024-09-11 21:42:33 +0530

    1109

--- pattern/palindrome.java
+++ pattern/palindrome.java
@@ -1,6 +1,8 @@
 public class palindrome {
     public static void main(String[] args) {
+        for(int k = 1; )
         for (int i = 1; i <= 5; i++) {
+
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
