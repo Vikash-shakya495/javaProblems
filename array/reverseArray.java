fec1ab30060ac684e723df5b618247d307454fa2
Vikash Shakya <vikashshakya367@gmail.com>
2024-09-25 13:44:04 +0530

    2509

--- array/reverseArray.java
+++ array/reverseArray.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class reverseArray {
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] temp = new int[arr.length];
+        for(int i = 0; i < temp.length; i++){
+            temp[i] = arr[temp.length -i -1];
+        }
+        System.out.println(Arrays.toString(temp));
     }
-}
+
+    // public static void swap(int[] arr ){
+    }
--- string/stringExample1.java
+++ string/stringExample1.java
@@ -3,9 +3,14 @@
 public class stringExample1 {
     public static void main(String[] args) {
         // String str1 = new String("delhi");
-        String str2 = "delih is an india";
-        System.out.println(str2.indexOf("d"));
+        // String str = "delih is an india";
+        // System.out.println(str2.indexOf("d"));
 
-        
+
+        String str1 = "Vikash";
+        String str2 = "Vikash";
+        System.out.println(str1 == str2);
+        // System.out.println(str1 === str2);
+        System.out.println(str1.equals(str2));
     }
 }
