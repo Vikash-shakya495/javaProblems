564aa671d75fb3e5476935dfa74f813b6ce12e92
Vikash Shakya <vikashshakya367@gmail.com>
2024-10-18 15:55:59 +0530

    1810

--- class/Bank.java
+++ class/Bank.java
@@ -1,21 +1,41 @@
-public class bank {
-    String acc_num = "0000";
-    String name = "cname";
-    String mob = "9999999990";
-    Double cur_bal = 0.0;
-    public create_account(String n, String m){
+public class Bank {
+    String acc_num = "0000";   // Default account number
+    String name = "cname";     // Default customer name
+    String mob = "9999999990"; // Default mobile number
+    Double cur_bal = 0.0;      // Current balance
+
+    // Constructor (optional, if you want to create an account at initialization)
+    public Bank() {}
+
+    // Create account method with correct syntax
+    public void createAccount(String n, String m) {
         name = n;
         mob = m;
+        // Optionally set account number, here for example as a static default
+        acc_num = "1234567890";  // Set to a default or random account number
     }
-    public void deposit(double bal){
-        cur_bal = cur_bal + bal;
+
+    // Deposit method
+    public void deposit(double bal) {
+        cur_bal += bal;
+        System.out.println("Deposited: " + bal);
     }
-    public void withdraw(double bal){
-        cur_bal = cur_bal - bal;
+
+    // Withdraw method with balance check
+    public void withdraw(double bal) {
+        if (bal <= cur_bal) {
+            cur_bal -= bal;
+            System.out.println("Withdrawn: " + bal);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
     }
-    public void display(){
+
+    // Display method to show account details
+    public void display() {
+        System.out.println("Account Holder: " + name);
+        System.out.println("Mobile Number: " + mob);
         System.out.println("Account Number: " + acc_num);
         System.out.println("Current Balance: " + cur_bal);
     }
 }
-
--- class/bankSystem.java
+++ class/bankSystem.java
@@ -1,5 +1,9 @@
 public class bankSystem {
-    public static void main(String[] args){
-        Bank b = new Bank();
+    public static void main(String[] args) {
+        Bank b = new Bank();  // Create a new Bank object
+        b.createAccount("Vikash", "9090909090");  // Create account
+        b.deposit(100.0);  // Deposit amount
+        b.withdraw(50.0);  // Withdraw amount
+        b.display();  // Display account details
     }
 }
