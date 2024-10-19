f2ddad953cd407b47beaf66b216edff7813e3823
Vikash Shakya <vikashshakya367@gmail.com>
2024-10-19 22:54:28 +0530

    1910

--- class/Student.java
+++ class/Student.java
@@ -1,20 +1,26 @@
- class Student {
- Student {
+// Student.java
+public class Student {
     int student_Id;
     String student_name;
     double age;
     char grade;
+
+    // Constructor (optional, Java provides a default constructor if none is defined)
+    public Student() {}
+
+    // Setter method
+    public void set(int id, String name, double a, char g) {
+        student_Id = id;
+        student_name = name;
+        age = a;
+        grade = g;
+    }
+
+    // Getter method
+    public void get() {
+        System.out.println("Student id : " + student_Id);
+        System.out.println("Student Name : " + student_name);
+        System.out.println("Student age : " + age);
+        System.out.println("Student grade : " + grade);
+    }
 }
-public void set(int id,String name, double a, char g){
-    student_id = id;
-    student_name = name;
-    age = a;
-    grade = g;
-}
-public void get(){
-    System.out.println("Student id : " + student_id);
-    System.out.println("Student Name : " + student_name);
-    System.out.println("Student age : " + age);
-    System.out.println("Student grade : " + grade);
-}
- }
