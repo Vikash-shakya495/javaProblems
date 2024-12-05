e3000de0c57b7e28ad89454fb8c8723719027fb7
Vikash Shakya <vikashshakya367@gmail.com>
2024-12-05 10:34:51 +0530

    0512

---
+++ packages/Message.java
@@ -0,0 +1,10 @@
+package packages.b;
+
+protected class Message {
+    public static void main(String[] args) {
+        
+    }
+    protected static void message(){
+        System.out.println("Hello, This course is awesome");
+    }
+}
