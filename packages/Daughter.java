68e7dfd1f176922982342771a345aabe5257035e
Vikash Shakya <vikashshakya367@gmail.com>
2024-12-05 10:52:38 +0530

    0512

---
+++ packages/Daughter.java
@@ -0,0 +1,11 @@
+package packages.abstract;
+public class Daughter extends Parent {
+    @Override
+    void career() {
+        System.out.println("I am going to become a coder");
+    }
+    @Override
+    void partner() {
+        System.out.println("My partner is Ironman");
+    }
+}
---
+++ packages/Son.java
@@ -0,0 +1,11 @@
+package packages.abstract;
+public class Son extends Parent {
+    @Override
+    void career() {
+        System.out.println("I am going to be a doctor");
+    }
+    @Override
+    void partner() {
+        System.out.println("My partner is Pepper Potts");
+    }
+}
