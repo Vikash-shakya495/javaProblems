29396e3d485ef85408bdd393226697132b5603ea
Vikash Shakya <vikashshakya367@gmail.com>
2024-12-05 10:33:18 +0530

    0512

---
+++ packages/Greeting.java
@@ -0,0 +1,9 @@
+package packages.a;
+import static packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("This is package.a.greeting file");
+        message();
+    }
+}
